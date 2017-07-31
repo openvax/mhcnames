17b86c2a3122ca7674be2b6f81dce27ca7631be8
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2017-07-31 13:43:38 -0400

    added pair inference option to normalize_, added unit tests

--- test/test_human_class2.py
+++ test/test_human_class2.py
@@ -30,3 +30,18 @@ def test_human_class2_alpha_beta():
                  "DPA10105/DPB110001"]:
         eq_(normalize_allele_name(name), expected)
         eq_(compact_allele_name(name), expected_compact)
+
+def test_alpha_chain_inference_DP():
+    # example of common DP haplotype from wikipedia article on HLA-DP
+    just_beta = "HLA-DPB1*04:01"
+    expected = "HLA-DPA1*01:03-DPB1*04:01"
+    eq_(normalize_allele_name(just_beta, infer_class2_pair=True), expected)
+    eq_(normalize_allele_name(just_beta, infer_class2_pair=False), just_beta)
+
+def test_alpha_chain_inference_DQ():
+    # example of common DQ haplotype from wikipedia article on HLA-DQ
+    just_beta = "HLA-DQB1*06:02"
+    expected = "HLA-DQA1*01:02-DQB1*06:02"
+    eq_(normalize_allele_name(just_beta, infer_class2_pair=True), expected)
+    eq_(normalize_allele_name(just_beta, infer_class2_pair=False), just_beta)
+
