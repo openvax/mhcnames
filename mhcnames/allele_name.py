c139884e2befd72b17a182fbca0ec4600724f7e2
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-02-21 16:55:00 -0500

    fixed swine parsing

--- mhcnames/allele_name.py
+++ mhcnames/allele_name.py
@@ -130,12 +130,32 @@ def parse_allele_name(name, species_prefix=None):
 
     # skip initial separator
     sep, name = parse_separator(name)
-    # If all that's left is e.g. "0201" then only parse the
-    # first two digits as the family code. Also, human Class I alleles
-    # seem to be exceptional in that they have only 2 digit allele
-    # families but 3 digit allele codes
-    # (other species like sheep have 3 digits followed by 2 digits)
-    if len(name) == 4 or (species == "HLA" and gene in ("A", "B", "C")):
+
+    if species == "SLA":
+        if ":" in name:
+            parts = name.split(":")
+            if len(parts) != 2:
+                raise AlleleParseError(
+                    "Unexpected number of ':' characters in '%s'" % original)
+            family, name = parts
+        elif len(name) < 2:
+                raise AlleleParseError("Unable to parse '%s'" % original)
+        elif len(name) == 2:
+            # parse sequences like SLA-1-HB as shorthand for SLA-1-HB01
+            family = name
+            name = "01"
+        else:
+            # the family names for pigs can be weirdly complicated
+            # such as 'w13sm' but the alleles still always
+            # end with two digits e.g. SLA-2*w13sm20
+            family = name[:-2]
+            name = name[-2:]
+    elif len(name) == 4 or (species == "HLA" and gene in ("A", "B", "C")):
+        # If all that's left is e.g. "0201" then only parse the
+        # first two digits as the family code. Also, human Class I alleles
+        # seem to be exceptional in that they have only 2 digit allele
+        # families but 3 digit allele codes
+        # (other species like sheep have 3 digits followed by 2 digits)
         family, name = parse_numbers(name, max_len=2)
     else:
         family, name = parse_numbers(name, max_len=3)
@@ -145,10 +165,11 @@ def parse_allele_name(name, species_prefix=None):
     allele_code, rest_of_text = parse_numbers(name)
 
     rest_of_text = rest_of_text.strip()
+
     if len(rest_of_text) > 0:
         raise AlleleParseError("The suffix '%s' of '%s' was not parsed" % (
-            original,
-            rest_of_text))
+            rest_of_text,
+            original))
 
     if len(family) == 1:
         family = "0" + family
@@ -156,9 +177,6 @@ def parse_allele_name(name, species_prefix=None):
         family = family[1:]
     if len(allele_code) == 0:
         allele_code = "01"
-    elif len(allele_code) == 1:
-        # change HLA-A*2:01 into HLA-A*02:01
-        allele_code = "0" + allele_code
     elif len(allele_code) == 3 and allele_code[0] == "0":
         # normalize HLA-A*02:001 into HLA-A*02:01
         allele_code = allele_code[1:]
--- test/test_swine.py
+++ test/test_swine.py
@@ -5,6 +5,11 @@ from mhcnames import (
 )
 from nose.tools import eq_
 
+def test_SLA_1_0101():
+    eq_(
+        parse_allele_name("SLA-1*01:01"),
+        AlleleName("SLA", "1", "01", "01"))
+
 def test_SLA_1_HB01():
     eq_(
         parse_allele_name("SLA-1-HB01"),
