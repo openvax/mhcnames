ecea3312ce366726e7bca896275431c45fd8835c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-02-22 13:53:36 -0500

    added SLA-10101 without seps

--- test/test_swine.py
+++ test/test_swine.py
@@ -5,7 +5,7 @@ from mhcnames import (
 )
 from nose.tools import eq_
 
-def test_SLA_1_0101():
+def test_SLA_1_0101_with_seps():
     eq_(
         parse_allele_name("SLA-1*01:01"),
         AlleleName("SLA", "1", "01", "01"))
@@ -14,3 +14,8 @@ def test_SLA_1_HB01():
     eq_(
         parse_allele_name("SLA-1-HB01"),
         AlleleName("SLA", "1", "HB", "01"))
+
+def test_SLA_1_0101_no_seps():
+    eq_(
+        parse_allele_name("SLA-10101"),
+        AlleleName("SLA", "1", "01", "01"))
