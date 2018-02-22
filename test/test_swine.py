1866500b8b509a0a1a7131ba130310c54f3ce3c4
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-02-22 13:58:08 -0500

    added names from Analyzing the genetic characteristics and function of the swine leukocyte antigen 2 gene in a Chinese inbreed of pigs

--- test/test_swine.py
+++ test/test_swine.py
@@ -1,3 +1,8 @@
+"""
+Pig allele names from:
+    Analyzing the genetic characteristics and function of the swine
+    leukocyte antigen 2 gene in a Chinese inbreed of pigs
+"""
 
 from mhcnames import (
     parse_allele_name,
@@ -19,3 +24,15 @@ def test_SLA_1_0101_no_seps():
     eq_(
         parse_allele_name("SLA-10101"),
         AlleleName("SLA", "1", "01", "01"))
+
+def test_SLA_2_07we01():
+    eq_(parse_allele_name("SLA-2*07we01"),
+        AlleleName("SLA", "2", "07we", "01"))
+
+def test_SLA_2_jh01():
+    eq_(parse_allele_name("SLA-2*jh01"),
+        AlleleName("SLA", "2", "jh", "01"))
+
+def test_SLA_2_w09pt22():
+    eq_(parse_allele_name("SLA-2*w09pt22"),
+        AlleleName("SLA", "2", "w09pt", "22"))
